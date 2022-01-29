18c558e403977a61ee7119578fa8f9a244f6027a
Italo Matos <italomatos@gmail.com>
2022-01-29 19:20:26 -0300

    add cancancan to template

--- template.rb
+++ template.rb
@@ -1,9 +1,17 @@
 # template.rb
 gem 'devise'
 gem 'rails_admin'
+gem 'cancancan'
+
+gsub_file "Gemfile", /^gem\s+["']sqlite3["'].*$/,''
 
 gem_group :development, :test do
   gem "rspec-rails"
+  gem 'sqlite3', '~> 1.4'
+end
+
+gem_group :production do
+  gem "pg"
 end
 
 devise_model_name = ask("Qual o nome do modelo de usuário de autenticação? Exemplo: User, Admin, etc. Deixando em branco será utilizado o modelo User")
@@ -14,6 +22,7 @@ after_bundle do
   rails_command("generate rails_admin:install")
   rails_command("generate devise:install")
   rails_command("generate rspec:install")
+  rails_command("generate cancan:ability")
   rails_command("generate devise #{devise_model_name}")
   rails_command("db:migrate")
 
@@ -23,6 +32,24 @@ after_bundle do
         warden.authenticate! scope: :#{devise_model_name.downcase}
       end
       config.current_user_method(&:current_#{devise_model_name.downcase})
+      config.authorize_with :cancancan
     end
   CODE
+
+  insert_into_file "app/models/ability.rb", "\n  can :access, :rails_admin \n  can :read, :dashboard \n", after: "def initialize(user)"
+    
+
+
+  git :init
+  git add: "."
+  git commit: "-m 'First commit!'"
+
+  if yes?("Publicar no Heroku?")
+    run "bundle lock --add-platform x86_64-linux"
+    git add: "."
+    git commit: "-m 'bundle lock --add-platform x86_64-linux'"
+    run "heroku create" 
+    run "git push heroku master" 
+    run "heroku run rails db:migrate" 
+  end
 end
